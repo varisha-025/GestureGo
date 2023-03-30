e92793da3d43b8d3610c04a75db655296809b75b
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2023-03-30 23:57:42 +0530

    created templates

--- GestureGo/SignToSpeech/views.py
+++ GestureGo/SignToSpeech/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def logout(request):
+    pass
+    # return render(request, 'logout.html')
