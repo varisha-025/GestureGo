67ef65a2f6c0ec67b4462212d0ba62b92b1ec7b9
Varisha Rashid <varisharashid@Varishas-Air.mshome.net>
2023-03-30 23:13:47 +0530

    Initial Setup

---
+++ GestureGo/GestureGo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GestureGo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GestureGo.settings')
+
+application = get_wsgi_application()
