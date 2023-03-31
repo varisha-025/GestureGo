d3027f5f517ab0dece00cb5cbe1703e018bc3cc4
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2023-03-31 14:01:32 +0530

    postgres config done

--- GestureGo/GestureGo/settings.py
+++ GestureGo/GestureGo/settings.py
@@ -77,21 +77,14 @@ WSGI_APPLICATION = 'GestureGo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'GestureGo',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin@123',
-#         'PORT': '5433',
-#         'HOST': 'localhost'
-#     }
-# }
-
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
