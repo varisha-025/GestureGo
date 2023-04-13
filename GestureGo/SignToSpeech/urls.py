7229ee1ec7d8938bd533a59b31017dcb19937f97
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2023-04-13 15:56:19 +0530

    include sign to speech urls and made changes

--- GestureGo/SignToSpeech/urls.py
+++ GestureGo/SignToSpeech/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('',views.dashboard,name="signtospeech_home"),
+     path('',views.dashboard,name="dashboard"),
+     path('prediction/',views.prediction,name="prediction"),
+     path('result/',views.result,name="result"),
 ]
+
+handler404 = "SignToSpeech.views.page_not_found"
--- GestureGo/SignToSpeech/views.py
+++ GestureGo/SignToSpeech/views.py
@@ -4,4 +4,15 @@ from django.http import JsonResponse
 
 
 def dashboard(request):
+    return render(request,'sign_to_speech/dashboard.html')
+
+
+def prediction(request):
     return JsonResponse({"message": "ok"})
+
+def result(request):
+    return JsonResponse({"message": "ok"})
+
+
+def page_not_found(request,exception):
+    return render(request,'sign_to_speech/404.html')
