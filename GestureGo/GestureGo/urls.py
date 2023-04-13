c4a912cea307bfb1673535671e336c0f15a33ea5
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2023-04-13 15:24:17 +0530

    arrange urls and views

--- GestureGo/GestureGo/urls.py
+++ GestureGo/GestureGo/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('SignToSpeech.urls')),
+    path('',views.home,name="home"),
+    path('login/',views.loginUser,name="login"),
+    path('logout/',views.logoutUser,name="logout"),
+    path('register/',views.registerUser,name="register"),
+    path('privacy/',views.privacy,name="privacy"),
+    path('signtospeech/',include('SignToSpeech.urls')),
 ]
