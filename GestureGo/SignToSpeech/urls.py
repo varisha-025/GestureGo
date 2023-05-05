770525b9ff617572d9ac4f721cf1d801f298c323
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2023-05-05 10:35:18 +0530

    changes

--- GestureGo/SignToSpeech/urls.py
+++ GestureGo/SignToSpeech/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.logout, name='logout'),
+    path('login/',views.loginUser,name="login"),
+    path('logout/',views.logoutUser,name="logout"),
+    path('register/',views.registerUser,name="register"),
     path('privacy/', views.privacy, name='privacy'),
     
 ]
 
-handler404 = "SignToSpeech.views.page_not_found"
+# handler404 = "SignToSpeech.views.page_not_found"
