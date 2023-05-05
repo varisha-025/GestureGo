071c2b75c590ab13d9ff9413981e77d791390927
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2023-05-05 11:16:42 +0530

    more changes

--- GestureGo/SignToSpeech/urls.py
+++ GestureGo/SignToSpeech/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path('login/',views.loginUser,name="login"),
     # path('logout/',views.logoutUser,name="logout"),
     # path('register/',views.registerUser,name="register"),
-    # path('privacy/', views.privacy, name='privacy'),
+    path('privacy/', views.privacy, name='privacy'),
     
 ]
