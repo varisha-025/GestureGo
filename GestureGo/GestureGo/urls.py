28da41fa1710592a90bbe18a56e5426533bf121c
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2023-05-16 22:55:35 +0530

    add speech to sign and other changes

--- GestureGo/GestureGo/urls.py
+++ GestureGo/GestureGo/urls.py
@@ -5,35 +5,33 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path("", views.home, name="home")
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path("", Home.as_view(), name="home")
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/login', permanent=False)),
-    
-    path('admin/', admin.site.urls),
-    path('home/',views.home,name="home"),
+    path("admin/", admin.site.urls),
+    path("",views.home,name="home"),
    
-    path('translation/',views.translation,name="translation"),
-    path('menu/', views.menu, name='menu'),
-    path('home/signtospeech/',include('SignToSpeech.urls')),
-    path('text_to_speech/',views.text_to_speech,name="text_to_speech"),
+    path("translation/",views.translation,name="translation"),
+    path("menu/", views.menu, name="menu"),
+    path("signtospeech/",include("SignToSpeech.urls")),
+    path("text_to_speech/",views.text_to_speech,name="text_to_speech"),
+    path("speech_to_sign/",views.speech_to_sign, name="speech_to_sign"),
 
-    path('login/',views.loginUser,name="login"),
-    path('logout/',views.logoutUser,name="logout"),
-    path('register/',views.registerUser,name="register"),
-    path('privacy/',views.privacy,name="privacy"),
-     path('asl/',views.asl,name="asl"),
+    path("login/",views.loginUser,name="login"),
+    path("logout/",views.logoutUser,name="logout"),
+    path("register/",views.registerUser,name="register"),
+    path("privacy/",views.privacy,name="privacy"),
+    path("asl/",views.asl,name="asl"),
     
 ]
--- GestureGo/GestureGo/views.py
+++ GestureGo/GestureGo/views.py
@@ -1,58 +1,55 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login,logout
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import nltk
+from django.contrib.staticfiles import finders
 
 
 def registerUser(request):
-    if request.user.is_authenticated:
-        return redirect('/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            # log the user in
+            return redirect('animation')
     else:
-        if request.method == 'POST':
-            form = UserCreationForm(request.POST)
-            username = request.POST.get('username')
-            password = request.POST.get('password1')
-            email = request.POST.get('email')
-            user = User.objects.create_user(username=username, password=password, email=email)
-            user.save()
-            messages.success(request, 'Account was created for ' + username)
-            if form.is_valid():
-                form.save()
-                username = form.cleaned_data.get('username')
-            return redirect('login')
-        else:
-            form = UserCreationForm()
-        context = {'form':form}
-        return render(request, 'register.html', context)
+        form = UserCreationForm()
+    return render(request,'register.html',{'form':form})
+
 
 def loginUser(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    else:
-        if request.method=="POST":
-            username= request.POST.get("username")
-            password= request.POST.get("password")
-            user_exist = User.objects.filter(username=username).exists()
-    
-            user = authenticate(request,username=username, password=password)
-            if user is not None and user_exist:
-                login(request,user)
-                return redirect("/")
-            else:
-                messages.error(request, 'Wrong username or password')
-                return render(request,'login.html')
-        return render(request,'login.html')
+	if request.method == 'POST':
+		form = AuthenticationForm(data=request.POST)
+		if form.is_valid():
+			#log in user
+			user = form.get_user()
+			login(request,user)
+			if 'next' in request.POST:
+				return redirect(request.POST.get('next'))
+			else:
+				return redirect('animation')
+	else:
+		form = AuthenticationForm()
+	return render(request,'login.html',{'form':form})
 
-@login_required(login_url='login')
 def logoutUser(request):
-	logout(request)
-	messages.info(request, "You have successfully logged out.") 
-	return redirect("login")
+    logout(request)
+    messages.info(request, "You have successfully logged out.") 
+    return redirect("login")
   
 def privacy(request):
     return render(request,'privacy.html')
+
 def asl(request):
     return render(request,'asl.html')
 
@@ -65,5 +62,92 @@ def menu(request):
 def text_to_speech(request):
     return render(request, 'text_to_speech.html')
 
+def speech_to_sign(request):
+    return render(request, 'speech_to_sign.html')
+
 def home(request):
     return render(request, 'layout/home.html')
+
+
+def speech_to_sign(request):
+	if request.method == 'POST':
+		text = request.POST.get('sen')
+		#tokenizing the sentence
+		text.lower()
+		#tokenizing the sentence
+		words = word_tokenize(text)
+
+		tagged = nltk.pos_tag(words)
+		tense = {}
+		tense["future"] = len([word for word in tagged if word[1] == "MD"])
+		tense["present"] = len([word for word in tagged if word[1] in ["VBP", "VBZ","VBG"]])
+		tense["past"] = len([word for word in tagged if word[1] in ["VBD", "VBN"]])
+		tense["present_continuous"] = len([word for word in tagged if word[1] in ["VBG"]])
+
+
+
+		#stopwords that will be removed
+		stop_words = set(["mightn't", 're', 'wasn', 'wouldn', 'be', 'has', 'that', 'does', 'shouldn', 'do', "you've",'off', 'for', "didn't", 'm', 'ain', 'haven', "weren't", 'are', "she's", "wasn't", 'its', "haven't", "wouldn't", 'don', 'weren', 's', "you'd", "don't", 'doesn', "hadn't", 'is', 'was', "that'll", "should've", 'a', 'then', 'the', 'mustn', 'i', 'nor', 'as', "it's", "needn't", 'd', 'am', 'have',  'hasn', 'o', "aren't", "you'll", "couldn't", "you're", "mustn't", 'didn', "doesn't", 'll', 'an', 'hadn', 'whom', 'y', "hasn't", 'itself', 'couldn', 'needn', "shan't", 'isn', 'been', 'such', 'shan', "shouldn't", 'aren', 'being', 'were', 'did', 'ma', 't', 'having', 'mightn', 've', "isn't", "won't"])
+
+
+
+		#removing stopwords and applying lemmatizing nlp process to words
+		lr = WordNetLemmatizer()
+		filtered_text = []
+		for w,p in zip(words,tagged):
+			if w not in stop_words:
+				if p[1]=='VBG' or p[1]=='VBD' or p[1]=='VBZ' or p[1]=='VBN' or p[1]=='NN':
+					filtered_text.append(lr.lemmatize(w,pos='v'))
+				elif p[1]=='JJ' or p[1]=='JJR' or p[1]=='JJS'or p[1]=='RBR' or p[1]=='RBS':
+					filtered_text.append(lr.lemmatize(w,pos='a'))
+
+				else:
+					filtered_text.append(lr.lemmatize(w))
+
+
+		#adding the specific word to specify tense
+		words = filtered_text
+		temp=[]
+		for w in words:
+			if w=='I':
+				temp.append('Me')
+			else:
+				temp.append(w)
+		words = temp
+		probable_tense = max(tense,key=tense.get)
+
+		if probable_tense == "past" and tense["past"]>=1:
+			temp = ["Before"]
+			temp = temp + words
+			words = temp
+		elif probable_tense == "future" and tense["future"]>=1:
+			if "Will" not in words:
+					temp = ["Will"]
+					temp = temp + words
+					words = temp
+			else:
+				pass
+		elif probable_tense == "present":
+			if tense["present_continuous"]>=1:
+				temp = ["Now"]
+				temp = temp + words
+				words = temp
+
+
+		filtered_text = []
+		for w in words:
+			path = w + ".mp4"
+			f = finders.find(path)
+			#splitting the word if its animation is not present in database
+			if not f:
+				for c in w:
+					filtered_text.append(c)
+			#otherwise animation of word
+			else:
+				filtered_text.append(w)
+		words = filtered_text;
+
+
+		return render(request,'speech_to_sign.html',{'words':words,'text':text})
+	else:
+		return render(request,'speech_to_sign.html')
